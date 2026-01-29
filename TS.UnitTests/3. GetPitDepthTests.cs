557390edd9736364d6cdd3b6863e272ce16a9aac
Denis Banda <DBanda@clientele.co.za>
2026-01-29 22:40:07 +0200

    Added an additonal test case to finalize Q2

---
+++ TS.UnitTests/3. GetPitDepthTests.cs
@@ -0,0 +1,16 @@
+﻿using TS.TechnicalTest;
+
+namespace TS.UnitTests;
+
+[TestClass]
+public class GetPitDepthTests
+{
+    [TestMethod]
+    public void Q2_Complete_BasicNominalTest()
+    {
+        var points = new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+        var result = DeepestPitAnswer.Solution(points);
+
+        Assert.AreEqual(2, result);
+    }
+}
