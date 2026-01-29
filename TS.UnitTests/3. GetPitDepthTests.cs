a6c3d7ccf35422f4645a06ba02241917f27c2657
Denis Banda <DBanda@clientele.co.za>
2026-01-29 22:42:12 +0200

    This will not compile until the method has been added to the DeepestPitAnswer.cs (GetPitDepth(int[] points))

--- TS.UnitTests/3. GetPitDepthTests.cs
+++ TS.UnitTests/3. GetPitDepthTests.cs
@@ -9,7 +9,7 @@ public class GetPitDepthTests
     public void Q2_Complete_BasicNominalTest()
     {
         var points = new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
-        var result = DeepestPitAnswer.Solution(points);
+        var result = DeepestPitAnswer.GetPitDepth(points);
 
         Assert.AreEqual(2, result);
     }
