ababd6037c535f34842863e0c18c0b16822a9d83
Denis Banda <DBanda@clientele.co.za>
2026-01-29 21:48:11 +0200

    Added exception handling to long sentence Q1, This should pass all the test casses.

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -4,22 +4,33 @@ namespace TS.TechnicalTest;
 public class LongestSentanceAnswer
 {
     public static int Solution(string sentencesValue)
-    { 
-        if (string.IsNullOrWhiteSpace(sentencesValue))
-            return 0;
-
-        var sentences = sentencesValue.Split('.', StringSplitOptions.RemoveEmptyEntries);
-
-        int maxWordCount = 0;
-
-        foreach (var sentence in sentences)
+    {
+        //I have returned to the first question to add exception handling before completing: DeepestPitAnswer.cs
+        try
         {
-            var words = sentence
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (string.IsNullOrWhiteSpace(sentencesValue))
+                return 0;
 
-            maxWordCount = Math.Max(maxWordCount, words.Length);
+            var sentences = sentencesValue.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            int maxWordCount = 0;
+
+            foreach (var sentence in sentences)
+            {
+                var words = sentence
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                maxWordCount = Math.Max(maxWordCount, words.Length);
+            }
+
+            return maxWordCount;
         }
-
-        return maxWordCount;
+        catch (Exception e)
+        {
+            //===========Bellow in sequence==========
+            //Stack holder requirements: real rime reports if required
+            //Logger to be added in my case as Denis Banda, i would normally register Serilog via DI in Program.cs, mostly implemented in Micro Service API hosted under IIS Either as a parent web app or a child web app in IIS with associated Service user aand App pool for each instance. | DB or File log (Preferablly file into a different net drive to avoid future performace issues) 
+        }
+        return 0;
     }
 }
